f79ba4285f0345ffb6f00e67e027daee536f9150
pajic <32738446+pajicadvance@users.noreply.github.com>
2026-08-10 20:14:24 +0200

    Fix swaps

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -10,6 +10,8 @@ stonecutter parameters {
     val (version, loader) = current.project.split('-', limit = 2)
     val versionFormatted = version.replace(".", "_")
     val loaderFormatted = loader.replaceFirstChar { it.uppercase(Locale.getDefault()) }
+    val modId = properties.get<String>("mod.id")
+    val modGroup = properties.get<String>("mod.group")
 
     properties {
         tags(version, loader)
@@ -19,10 +21,10 @@ stonecutter parameters {
         match(loader, "fabric", "neoforge")
     }
 
-    swaps["mod_id"] = "\"${properties.get<String>("mod.id")}\";"
-    swaps["version_util_import"] = "import me.pajic.modid.platform.version.Util${versionFormatted};"
+    swaps["mod_id"] = "\"${modId}\";"
+    swaps["version_util_import"] = "import ${modGroup}.${modId}.platform.version.Util${versionFormatted};"
     swaps["version_util_inst"] = "new Util${versionFormatted}();"
-    swaps["loader_util_import"] = "import me.pajic.modid.platform.${loader}.${loaderFormatted}LoaderUtil;"
+    swaps["loader_util_import"] = "import ${modGroup}.${modId}.platform.${loader}.${loaderFormatted}LoaderUtil;"
     swaps["loader_util_inst"] = "new ${loaderFormatted}LoaderUtil();"
     constants["release"] = properties.get<String>("mod.id") != "template"
     dependencies["fapi"] = properties.getOrNull<String>("deps.fabric_api") ?: "0"
